a6fd45727874df37a47e5a706e552a04c3a32d59
Rodolfo Barrios <rodolfo.9408@gmail.com>
2018-10-25 17:03:54 -0500

    Clase Customer
    Signed-off-by: Rodolfo Barrios <rodolfo.9408@gmail.com>

---
+++ src/app/customer.ts
@@ -0,0 +1,6 @@
+export class Customer {
+    constructor(public id: number,
+                public firstname: string,
+                public lastname: string,
+                public age: number){}
+}
